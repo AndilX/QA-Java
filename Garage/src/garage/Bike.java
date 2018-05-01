a1c8957027edc69bfeb56662d6e7720bd3a90ac8
Andil Y Aboubakari <Andil.Aboubakari@academytrainee.com>
2018-05-01 11:52:19 +0100

    add garage exercise

---
+++ Garage/src/garage/Bike.java
@@ -0,0 +1,34 @@
+package garage;
+
+public class Bike extends Vehicle {
+    private boolean hasSidecar;
+    protected Bike(String brand, String modelName, int modelYear) {
+        super(brand, modelName, modelYear);
+        this.hasSidecar=false;
+    }
+
+
+    @Override
+    public void setNumberOfWheels() {
+        this.numberOfWheels=2;
+    }
+
+    @Override
+    public double calculateBill() {
+        double total = 50;
+        if (hasSidecar){
+            total += 50;
+        }
+        return total;
+    }
+
+
+    public boolean isHasSidecar() {
+        return hasSidecar;
+    }
+
+    public void setHasSidecar(boolean hasSidecar) {
+        this.hasSidecar = hasSidecar;
+    }
+
+}
---
+++ Garage/src/garage/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Andil
+ * Garage exercise - Level 2 Intermediate
+ *
+ */
+package garage;
